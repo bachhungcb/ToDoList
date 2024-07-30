c40a2c02d9c9247ede25604abfea508df5332259
bachhungcb <bachhungcb@gmail.com>
2024-07-30 22:48:36 +0700

    trying to add navbar to project

---
+++ frontend/notes-app/src/pages/team.js
@@ -0,0 +1,20 @@
+// pages/team.js
+
+import React from "react";
+
+const Teams = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "centre",
+                alignItems: "centre",
+                height: "100vh",
+            }}
+        >
+            <h1>Welcome to GeeksforGeeks Team</h1>
+        </div>
+    );
+};
+
+export default Teams;
