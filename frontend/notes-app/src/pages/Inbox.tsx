72a5f5b0a9f008782f8e86ca76bf6f5d59bc9f93
bachhungcb <bachhungcb@gmail.com>
2024-08-04 17:21:13 +0700

    format file for better readibilioty

--- frontend/notes-app/src/pages/Inbox.tsx
+++ frontend/notes-app/src/pages/Inbox.tsx
@@ -1,4 +1,4 @@
-import "../css/Home.css";
+import "../css/Inbox.css";
 import { useEffect, useState, useRef } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -27,16 +27,13 @@ const Inbox = () => {
   const [Content, setContent] = useState("");
   const [startDate, setStartDate] = useState<Date | null>(new Date());
   const [notes, setNotes] = useState<Note[]>([]);
-  const fetchCountRef = useRef<number>(0); // Ref for counter
   useEffect(() => {
 
     const fetchNotes = async() =>{
       setIsLoading(true);
 
       try{
-        fetchCountRef.current++; // Increment counter
-        console.log(`fetchNotes has been called ${fetchCountRef.current} times`); // Log counter value
-
+        
         const response = await fetch(
                                 "http://localhost:5000/app/notes",
                                 ); //lay data tu BE, default method la GET
