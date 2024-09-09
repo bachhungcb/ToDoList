8d816d1e046fe74361cabbdac35610e1c35bfbb8
bachhungcb <bachhungcb@gmail.com>
2024-09-09 21:05:11 +0700

    .

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const app = express();
 
 const { connectToDatabase } = require('./src/config/databaseConfig.js');
 const notesRouter = require('./src/routes/CRUDRoutes.js');
 const getNotesRouter = require('./src/routes/getNotesRoutes.js');
 const PORT = process.env.PORT || 5000;
 
-const app = express();
-
-
 app.use(express.json());
 app.use(cors());
 
 app.use("/", notesRouter);
 app.use("/app", getNotesRouter);
-
 
 const startServer = async () => {
   await connectToDatabase(); // Connect to MongoDB before starting the server
