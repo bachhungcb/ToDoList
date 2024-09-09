c25d967ab291ddef5a6f26fb6502301763e60f6b
bachhungcb <bachhungcb@gmail.com>
2024-09-09 19:39:42 +0700

    awcwc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() =>{
+    const fetchHelloWorld = async()  =>{
+      const res = await axios.get("http://localhost:5000/app/notes");
+      console.log(">>check res: ", res);
+    }
+    fetchHelloWorld();
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Hello World
+    </>
   );
 }
 
