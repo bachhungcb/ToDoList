42a2a5d4de93e6fcad5e9021830cb090cdaa17b5
bachhungcb <bachhungcb@gmail.com>
2024-09-22 13:11:35 +0700

    get token from req.header

--- backend/src/middleware/delay.js
+++ backend/src/middleware/delay.js
@@ -1,5 +1,9 @@
 const delay = (req, res, next) =>{
     setTimeout(()=>{
+        if(req.headers.authorization){
+            const token = req.headers.authorization.split(' ')[1];
+            console.log(token);
+        }
         next();
     }, 3000)
 }
